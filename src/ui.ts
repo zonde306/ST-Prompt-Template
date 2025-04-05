dde72c755cba8dab014357e27e718970a17607ee
zonde306 <anonymous@localhost>
2025-04-05 21:47:32 +0800

    feat: help info (chinese)

--- src/ui.ts
+++ src/ui.ts
@@ -56,57 +56,81 @@ export async function init() {
                 </div>
                 
                 <div class="inline-drawer-content">
-                    <label class="flex-container">
-                        <input type="checkbox" id="pt_enabled"><span data-i18n="[PT] Enable">Enable Prompt template</span>
+                    <label class="flex-container" title="是否开启提示词模板扩展">
+                        <input type="checkbox" id="pt_enabled">
+                        <span data-i18n="[PT] Enable">Enable Prompt template</span>
+                        <span class="fa-solid fa-circle-question note-link-span"></span>
                     </label>
                     
-                    <label class="flex-container">
-                        <input type="checkbox" id="pt_generate_enabled"><span data-i18n="[PT] Enable Generate-time evaluation">Generate-time evaluation</span>
+                    <label class="flex-container" title="在生成时执行模板处理">
+                        <input type="checkbox" id="pt_generate_enabled">
+                        <span data-i18n="[PT] Enable Generate-time evaluation">Generate-time evaluation</span>
+                        <span class="fa-solid fa-circle-question note-link-span"></span>
                     </label>
                     
-                    <label class="flex-container">
-                        <input type="checkbox" id="pt_render_enabled"><span data-i18n="[PT] Enable Render-time evaluation">Render-time evaluation</span>
+                    <label class="flex-container" title="在渲染（楼层消息）执行模板处理">
+                        <input type="checkbox" id="pt_render_enabled">
+                        <span data-i18n="[PT] Enable Render-time evaluation">Render-time evaluation</span>
+                        <span class="fa-solid fa-circle-question note-link-span"></span>
                     </label>
                     
-                    <label class="flex-container">
-                        <input type="checkbox" id="pt_generate_before_enabled"><span data-i18n="[PT] Enable [GENERATE:BEFORE] evaluation">[GENERATE:BEFORE] evaluation</span>
+                    <label class="flex-container" title="启用 [GENERATE:BEFORE] 功能（详见文档）">
+                        <input type="checkbox" id="pt_generate_before_enabled">
+                        <span data-i18n="[PT] Enable [GENERATE:BEFORE] evaluation">[GENERATE:BEFORE] evaluation</span>
+                        <span class="fa-solid fa-circle-question note-link-span"></span>
                     </label>
                     
-                    <label class="flex-container">
-                        <input type="checkbox" id="pt_generate_after_enabled"><span data-i18n="[PT] Enable [GENERATE:AFTER] evaluation">[GENERATE:AFTER] evaluation</span>
+                    <label class="flex-container" title="启用 [GENERATE:AFTER] 功能（详见文档）">
+                        <input type="checkbox" id="pt_generate_after_enabled">
+                        <span data-i18n="[PT] Enable [GENERATE:AFTER] evaluation">[GENERATE:AFTER] evaluation</span>
+                        <span class="fa-solid fa-circle-question note-link-span"></span>
                     </label>
                     
-                    <label class="flex-container">
-                        <input type="checkbox" id="pt_render_before_enabled"><span data-i18n="[PT] Enable [RENDER:BEFORE] evaluation">[RENDER:BEFORE] evaluation</span>
+                    <label class="flex-container" title="启用 [RENDER:BEFORE] 功能（详见文档）">
+                        <input type="checkbox" id="pt_render_before_enabled">
+                        <span data-i18n="[PT] Enable [RENDER:BEFORE] evaluation">[RENDER:BEFORE] evaluation</span>
+                        <span class="fa-solid fa-circle-question note-link-span"></span>
                     </label>
                     
-                    <label class="flex-container">
-                        <input type="checkbox" id="pt_render_after_enabled"><span data-i18n="[PT] Enable [RENDER:AFTER] evaluation">[RENDER:AFTER] evaluation</span>
+                    <label class="flex-container" title="启用 [RENDER:AFTER] 功能（详见文档）">
+                        <input type="checkbox" id="pt_render_after_enabled">
+                        <span data-i18n="[PT] Enable [RENDER:AFTER] evaluation">[RENDER:AFTER] evaluation</span>
+                        <span class="fa-solid fa-circle-question note-link-span"></span>
                     </label>
                     
                     <hr>
                     
-                    <label class="flex-container">
-                        <input type="checkbox" id="pt_autosave_enabled"><span data-i18n="[PT] Enable Autosave">Save variables after updating</span>
+                    <label class="flex-container" title="模板处理后，若变量发生变化，则立即保存（到服务器）">
+                        <input type="checkbox" id="pt_autosave_enabled">
+                        <span data-i18n="[PT] Enable Autosave">Save variables after updating</span>
+                        <span class="fa-solid fa-circle-question note-link-span"></span>
                     </label>
                     
-                    <label class="flex-container">
-                        <input type="checkbox" id="pt_preload_worldinfo"><span data-i18n="[PT] Enable Preload">Preload world info</span>
+                    <label class="flex-container" title="打开角色卡时执行世界书（用于读取define）">
+                        <input type="checkbox" id="pt_preload_worldinfo">
+                        <span data-i18n="[PT] Enable Preload">Preload world info</span>
+                        <span class="fa-solid fa-circle-question note-link-span"></span>
                     </label>
                     
                     
-                    <label class="flex-container">
-                        <input type="checkbox" id="pt_code_blocks"><span data-i18n="[PT] Enable code blocks">Evaluate inside a code block</span>
+                    <label class="flex-container" title="允许在&lt;pre&gt;内执行模板处理（可能会冲突）">
+                        <input type="checkbox" id="pt_code_blocks">
+                        <span data-i18n="[PT] Enable code blocks">Evaluate inside a code block</span>
+                        <span class="fa-solid fa-circle-question note-link-span"></span>
                     </label>
                     
                     <hr>
                     
-                    <label class="flex-container">
-                        <input type="checkbox" id="pt_strict_enabled"><span data-i18n="[PT] Enable strict mode">Use strict mode</span>
+                    <label class="flex-container" title="执行模板处理时使用 Javascript 严格模式">
+                        <input type="checkbox" id="pt_strict_enabled">
+                        <span data-i18n="[PT] Enable strict mode">Use strict mode</span>
+                        <span class="fa-solid fa-circle-question note-link-span"></span>
                     </label>
                     
-                    <label class="flex-container">
-                        <input type="checkbox" id="pt_debug_enabled"><span data-i18n="[PT] Enable debug logging">Enable debug logging</span>
+                    <label class="flex-container" title="发生错误时输出更详细的信息">
+                        <input type="checkbox" id="pt_debug_enabled">
+                        <span data-i18n="[PT] Enable debug logging">Enable debug logging</span>
+                        <span class="fa-solid fa-circle-question note-link-span"></span>
                     </label>
                 </div>
             </div>
