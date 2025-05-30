bb5b96c410e53b50bc87e2b97dc229a523ea0eea
zonde306 <anonymous@localhost>
2025-05-31 00:46:32 +0800

    fix: typing

---
+++ src/globals.d.ts
@@ -0,0 +1,7 @@
+// globals.d.ts
+/** @type {import('lodash').LoDashStatic} */
+declare const _: any;
+/** @type {import('jquery').JQueryStatic} */
+declare const $: any;
+/** @type {import('toastr').Toastr} */
+declare const toastr: any;
