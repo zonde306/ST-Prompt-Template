17ac278b07be5cf458bd898667542cb98feacfd1
zonde306 <anonymous@localhost>
2025-07-27 18:22:03 +0800

    fix: activation multiple WI

--- src/modules/handler.ts
+++ src/modules/handler.ts
@@ -58,15 +58,19 @@ async function handleWorldInfoLoaded(data: WorldInfoLoaded) {
     for(const entry of getActivateWorldInfo()) {
         if(entry.disable || entry.decorators.includes('@@dont_activate')) {
             data.chatLore.push({ ...entry, disable: false, decorators: []});
+            console.debug(`[Prompt Template] activate ${entry.world}.${entry.comment} #${entry.uid}`);
             continue;
         }
 
         const idx = enabled.findIndex(x => x.world === entry.world && x.uid === entry.uid);
         if(idx < 0) {
             data.chatLore.push({ ...entry, disable: false, decorators: [] });
+            console.debug(`[Prompt Template] activate ${entry.world}.${entry.comment} #${entry.uid}`);
             continue;
         }
     }
+
+    deactivateActivateWorldInfo();
 }
 
 async function handleGenerateAfter(data: GenerateAfterData) {
