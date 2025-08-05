3813e1e8b1cb422fdd9a8320fb0e759bbfe9e7d7
zonde306 <anonymous@localhost>
2025-08-05 14:13:39 +0800

    fix: regexp depth of getChatMessage and getChatMessages

--- src/function/chat.ts
+++ src/function/chat.ts
@@ -2,11 +2,14 @@ import { chat, substituteParams } from "../../../../../../script.js";
 import { getRegexedString, regex_placement } from "../../../../regex/engine.js";
 import { Message } from "../modules/defines";
 
-function processMessage(message: Message) : string {
+function processMessage(message?: Message & { id: number }) : string {
+    if(message == null)
+        return '';
     const placement = message.is_user ? regex_placement.USER_INPUT : regex_placement.AI_OUTPUT;
     return substituteParams(getRegexedString(message.mes, placement, {
         characterOverride: message.name,
-        isPrompt: true
+        isPrompt: true,
+        depth: chat.length - message.id - 1, // [0...6].length === 7
     }));
 }
 
@@ -18,8 +21,10 @@ function processMessage(message: Message) : string {
  * @returns Message content
  */
 export function getChatMessage(idx: number, role?: 'user' | 'assistant' | 'system') : string {
-    const messages : Message[] = chat.filter(x => !role || (role === 'user' && x.is_user) || (role === 'system' && x.is_system) || (role === 'assistant' && !x.is_user && !x.is_system));
-    return processMessage(messages[idx > -1 ? idx : messages.length + idx] || '');
+    const messages : (Message & { id: number })[] = Object.entries(chat)
+        .map(([id, obj]) => ({ ...obj, id: Number(id) }))
+        .filter(x => !role || (role === 'user' && x.is_user) || (role === 'system' && x.is_system) || (role === 'assistant' && !x.is_user && !x.is_system));
+    return processMessage(messages[idx > -1 ? idx : messages.length + idx]);
 }
 
 /**
@@ -59,34 +64,40 @@ export function getChatMessages(startOrCount: number = chat.length,
     role?: 'user' | 'assistant' | 'system') : string[] {
     if(endOrRole == null) {
         if(startOrCount > 0) {
-            return chat
+            return Object.entries(chat)
+                .map(([idx, obj]) => ({ ...obj, id: Number(idx) }))
                 .slice(0, startOrCount)
                 .map(processMessage);
         } else if(startOrCount < 0) {
-            return chat
+            return Object.entries(chat)
+                .map(([idx, obj]) => ({ ...obj, id: Number(idx) }))
                 .slice(startOrCount)
                 .map(processMessage);
         }
     } else if(typeof endOrRole === 'string') {
         if(startOrCount > 0) {
-            return chat
+            return Object.entries(chat)
+                .map(([idx, obj]) => ({ ...obj, id: Number(idx) }))
                 .filter(x => (endOrRole === 'user' && x.is_user) || (endOrRole === 'system' && x.is_system) || (endOrRole === 'assistant' && !x.is_user && !x.is_system))
                 .slice(0, startOrCount)
                 .map(processMessage);
         } else if(startOrCount < 0) {
-            return chat
+            return Object.entries(chat)
+                .map(([idx, obj]) => ({ ...obj, id: Number(idx) }))
                 .filter(x => (endOrRole === 'user' && x.is_user) || (endOrRole === 'system' && x.is_system) || (endOrRole === 'assistant' && !x.is_user && !x.is_system))
                 .slice(startOrCount)
                 .map(processMessage);
         }
     } else if(typeof endOrRole === 'number') {
         if(startOrCount > 0) {
-            return chat
+            return Object.entries(chat)
+                .map(([idx, obj]) => ({ ...obj, id: Number(idx) }))
                 .filter(x => !role || (role === 'user' && x.is_user) || (role === 'system' && x.is_system) || (role === 'assistant' && !x.is_user && !x.is_system))
                 .slice(startOrCount, endOrRole)
                 .map(processMessage);
         } else if(startOrCount < 0) {
-            return chat
+            return Object.entries(chat)
+                .map(([idx, obj]) => ({ ...obj, id: Number(idx) }))
                 .filter(x => !role || (role === 'user' && x.is_user) || (role === 'system' && x.is_system) || (role === 'assistant' && !x.is_user && !x.is_system))
                 .slice(startOrCount, endOrRole)
                 .map(processMessage);
