8fed0f0f1fc67bb011af3b8743d2200b1a540469
zonde306 <anonymous@localhost>
2026-07-26 15:20:28 +0800

    fix: IIFE error

--- src/modules/handler.ts
+++ src/modules/handler.ts
@@ -118,7 +118,7 @@ async function handleWorldInfoLoaded(data: WorldInfoLoaded) {
                     throw error;
                 }
             } else if (!entry.disable && hdl.isPrivateEntry) {
-                data[type][i] = { ...entry, content: `${hdl.decoratorContent}\n<% (()=>{%>${hdl.cleanContent}<%})(); %>` };
+                data[type][i] = { ...entry, content: `${hdl.decoratorContent}\n<% await (async()=>{%>${hdl.cleanContent}<%})(); %>` };
                 console.debug(`[Prompt Template] Mark ${type} of ${entry.world}/${entry.comment}/${entry.uid} as private`);
             }
 
